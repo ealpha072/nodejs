ffd1cfe8fad9db6f138c2b8aac848a756e97005d
ealpha072 <ealpha072@gmail.com>
2020-12-20 13:43:49 +0300

    Code for opening req file with 404 error option

--- firstnode.js
+++ firstnode.js
@@ -72,3 +72,42 @@ fs.rename('mynewfile1.txt', 'myrenamedfile.txt', function (err) {
   console.log('File Renamed!');
   //renames a file
 });
+
+
+
+// URL MODULE
+    //url module splits a web address into readable parts
+
+    var url = require('url') //includes the url module
+
+    var address = 'http://localhost:8080/default.htm?year=2017&month=february';
+    var q = url.parse(address,true);
+
+    //url.parse returns a url object with each part of address as property
+
+    console.log(q.host); //returns 'localhost:8080'
+    console.log(q.pathname); //returns '/default.htm'
+    console.log(q.search); //returns '?year=2017&month=february'
+    var qdata = q.query; //returns an object: { year: 2017, month: 'february' }
+
+//nodejs file server
+    //the below code opens the requested file and returns content to the client
+    //if ther is an error, 404 is thrown
+
+var http = require('http');
+var url = require('url');
+var fs =require('fs')
+
+http.createServer(function(req,res){
+  var q = url.parse(req.url, true),
+  var filename ='.'+ q.pathname;
+  fs.readFile(filename,function(err,data){
+    if(err){
+      res.writeHead(404,{'Content-Type': 'text/html'});
+      return res.end()
+    }
+    res.writeHead(200,{'Content-Type':'text/html'})
+    res.write(data)
+    return res.end()
+  })
+}).listen(8080)
